eb532f7a3f9971316d684b54469fb6c9cf6c2a7d
MustafaShalan10 <104972789+MustafaShalan10@users.noreply.github.com>
2022-05-05 11:41:30 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> " last project session");
+		get("/", (req, res) -> " session 3 ");
 	}
 }
